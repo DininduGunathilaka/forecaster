8a9a8b9870087e1e283222da5d1ae9a6d73348ee
Dinindu Gunathilaka <dinindugunathilaka@gmail.com>
2024-01-07 23:20:17 +0530

    create remove command arguements

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,52 +1,59 @@
-const chalk = require('chalk')
-const yargs = require('yargs')
-const note = require('./notes')
+const chalk = require("chalk");
+const yargs = require("yargs");
+const note = require("./notes");
 //Create add command
 yargs.command({
-    command : 'add',
-    describe : 'Add a new note',
-    builder :{
-        title : {
-            describe:'Note Title',
-            demandOption:true,
-            type:'string'
-        },
-        body : {
-            describe:'Note Body',
-            demandOption:true,
-            type:'string'
-        }
+  command: "add",
+  describe: "Add a new note",
+  builder: {
+    title: {
+      describe: "Note Title",
+      demandOption: true,
+      type: "string",
     },
-    handler: function(argv){
-        note.addNotes(argv.title, argv.body)
-    }
-})
+    body: {
+      describe: "Note Body",
+      demandOption: true,
+      type: "string",
+    },
+  },
+  handler: function (argv) {
+    note.addNotes(argv.title, argv.body);
+  },
+});
 
 //Create remove command
 
 yargs.command({
-    command: 'remove',
-    describe : 'Remove a note',
-    handler : function(){
-        console.log('Removing the note')
-    }
-})
+  command: "remove",
+  describe: "Remove a note",
+  builder: {
+    title: {
+      describe: "Note title",
+      demandOption:true,
+      type:'string'
+    },
+  },
+  handler: function () {
+    console.log("Removing the note");
+  },
+});
 
 //Create list command
 yargs.command({
-    command: 'list',
-    describe: 'List down all the notes',
-    handler: function(){
-        console.log('List down all the notes')
-    }
-})
+  command: "list",
+  describe: "List down all the notes",
+  handler: function () {
+    console.log("List down all the notes");
+  },
+});
 
 yargs.command({
-    command: 'read',
-    describe: 'Read a notes',
-    handler: function(){
-        console.log('Read a notes')
-    }
-})
+  command: "read",
+  describe: "Read a notes",
+  handler: function () {
+    console.log("Read a notes");
+  },
+});
 
-yargs.parse()
+yargs.parse();
