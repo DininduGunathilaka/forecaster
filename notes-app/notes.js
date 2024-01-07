94b6cd2ebbbabd97be808e718026de850bda795a
Dinindu Gunathilaka <dinindugunathilaka@gmail.com>
2024-01-07 23:42:11 +0530

    Completed the rmoving note functionality

--- notes-app/notes.js
+++ notes-app/notes.js
@@ -1,5 +1,5 @@
 const fs = require("fs");
-const chalk = require('chalk')
+const chalk = require("chalk");
 
 const getNotes = function () {
   return "Your notes...";
@@ -7,29 +7,42 @@ const getNotes = function () {
 
 const addNotes = function (title, body) {
   const notes = loadNotes();
-  const duplicateNotes = notes.filter(function(note){
-    return note.title===title
-  })
-  if(duplicateNotes.length===0){
+  const duplicateNotes = notes.filter(function (note) {
+    return note.title === title;
+  });
+  if (duplicateNotes.length === 0) {
     notes.push({
-      title:title,
-      body:body
-    })
-    saveNotes(notes)
-    console.log(chalk.blue('New note added!'))
-  }else{
-    console.log(chalk.red('Title already exists!'))
+      title: title,
+      body: body,
+    });
+    saveNotes(notes);
+    console.log(chalk.blue("New note added!"));
+  } else {
+    console.log(chalk.red("Title already exists!"));
   }
 };
 
-const removeNote = function(title){
-  console.log(chalk.green(title+' note removed successfully'))
-}
+const removeNote = function (title) {
+  const notes = loadNotes();
+  let x=0;
+  const noteRemoved = notes.filter(function (note) {
+    if(note.title===title){
+      x++
+    }
+    return note.title != title;
+  });
+  if (x > 0) {
+    saveNotes(noteRemoved);
+    console.log(chalk.green(title + " note removed successfully"));
+  }else{
+  console.log(chalk.red('No notes found with the title: '+title))
+  }
+};
 
-const saveNotes = function(notes){
-  const noteJSON = JSON.stringify(notes)
-  fs.writeFileSync('note.json',noteJSON)
-}
+const saveNotes = function (notes) {
+  const noteJSON = JSON.stringify(notes);
+  fs.writeFileSync("note.json", noteJSON);
+};
 
 const loadNotes = function () {
   try {
@@ -37,12 +50,12 @@ const loadNotes = function () {
     const dataString = dataBuffer.toString();
     return JSON.parse(dataString);
   } catch (e) {
-    return []
+    return [];
   }
 };
 
 module.exports = {
   getNodes: getNotes,
   addNotes: addNotes,
-  removeNote:removeNote
+  removeNote: removeNote,
 };
